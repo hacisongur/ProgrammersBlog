d7ae295cd58d91e865c17041441c214865e04c09
hacisongur <hacisongur@inspiria.vip>
2022-11-10 23:53:06 +0300

    Add project files.

---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;//oluşturulma tarihi
+        public virtual  DateTime ModifiedDate { get; set; } = DateTime.Now;//Düzenleme Tarihi
+        public virtual bool IsDeleted { get; set; } = false;//silindi mi?
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; } = "Admin";
+        public virtual  string ModifiedByName { get; set; } = "Admin";
+        public virtual string Note { get; set; } 
+    }
+}
