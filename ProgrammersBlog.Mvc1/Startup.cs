7774151ef598ff3e0baf527c72dc878809a6c152
hacisongur <hacisongur@inspiria.vip>
2022-11-17 00:57:02 +0300

    mvc

--- ProgrammersBlog.Mvc1/Startup.cs
+++ ProgrammersBlog.Mvc1/Startup.cs
@@ -24,7 +24,8 @@ namespace ProgrammersBlog.Mvc1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddAutoMapper(typeof(Startup));
             services.LoadMyServices();
         }
 
